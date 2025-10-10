ad18a3783457947982b8ea744a939d05ac631fd8
venkat <venkatmariserla21@gmail.com>
2025-10-11 03:30:46 +0530

    Initial commit for render.yml

--- dbs_devops2_clean/frontend/feedback-frontend/src/api.js
+++
@@ -1,5 +0,0 @@
-export const fetchDashboardData = async () => {
-  const response = await fetch("http://localhost:8085/api/dashboard");
-  if (!response.ok) throw new Error("Failed to fetch dashboard data");
-  return response.json();
-};
